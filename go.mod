4a017356ecf226f3a72e943c3df3503d7f93c9a0
Thiagojm <tjm.plastica@gmail.com>
2022-11-30 17:36:38 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go_rng
+
+go 1.19
+
+require go.bug.st/serial v1.4.1
+
+require (
+	github.com/creack/goselect v0.1.2 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.bug.st/serial"
+)
+
+// function to make a file name based on the current time and date
+func makeFileName(sample_size, interval_value int) string {
+	t := time.Now()
+	return fmt.Sprintf("%d%02d%02d-%02d%02d%02d_trng_s%d_i%d.bin", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), sample_size, interval_value)
+}
+
+func collectData(s serial.Port, sample_size int, interval_value int) {
+	file_name := makeFileName(sample_size, interval_value)
+	block_size := sample_size / 8
+	buff := make([]byte, block_size)
+	num_loop := 1
+	total_bytes := 0
+	for {
+		start := time.Now()
+		total_bytes += block_size
+		fmt.Printf("Collecting data - Loop: %d - Total bytes collected: %d\n", num_loop, total_bytes)
+		// Reads up to 100 bytes
+		n, err := s.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n == 0 {
+			fmt.Println("\nEOF")
+		}
+
+		// Open a file to write the data to
+		file, err := os.OpenFile(file_name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+		if err != nil {
+			fmt.Printf("Could not open %s\n", file_name)
+			return
+		}
+
+		defer file.Close()
+
+		_, err2 := file.Write(buff)
+
+		if err2 != nil {
+			fmt.Printf("Could not write text to %s\n", file_name)
+		}
+
+		// sz := cap(buff)
+		// fmt.Printf("The cap is: %v \n", sz)
+
+		// fmt.Println(buff)
+
+		// for _, i := range buff[:n] {
+		// 	fmt.Printf("% 08b", i) // prints 00000000 11111101
+		// }
+		// fmt.Println()
+		num_loop += 1
+		// sleep for 1 second
+		elapsed := time.Since(start)
+		time_to_sleep := (time.Duration(interval_value) * time.Second) - elapsed
+		time.Sleep(time_to_sleep)
+	}
+}
+
+func main() {
+
+	// Retrieve the port list
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+		return
+	}
+
+	// Print the list of detected ports
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	// Open the first serial port detected at 9600bps N81
+	mode := &serial.Mode{
+		BaudRate: 300,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port, err := serial.Open(ports[0], mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sample_size, interval_value := 2048, 1
+	collectData(port, sample_size, interval_value)
+
+}
