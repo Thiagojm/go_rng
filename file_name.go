242b2080c8be17e16fd039df0c2c09dc384d36e4
Thiagojm <tjm.plastica@gmail.com>
2022-12-07 18:27:04 -0300

    V 1.0

--- file_name.go
+++ file_name.go
@@ -11,7 +11,7 @@ import (
 func makeFileName(device string, sample_size int, interval_value int) (string, string) {
 	t := time.Now()
 	file_name := fmt.Sprintf("%d%02d%02d-%02d%02d%02d_%s_s%d_i%d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), device, sample_size, interval_value)
-	fmt.Printf(pterm.LightRed("Using file name: %v\n"), file_name)
+	fmt.Printf(pterm.LightMagenta("Using file name: %v\n"), file_name)
 	bin_file := file_name + ".bin"
 	csv_file := file_name + ".csv"
 	return bin_file, csv_file
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	sample_size, interval_value := 2048, 1
+	sample_size, interval_value := 1, 1
 	// Retrieve the port list
 	p := get_correct_port()
 
@@ -20,17 +20,22 @@ func main() {
 		DataBits: 8,
 		StopBits: serial.OneStopBit,
 	}
-
-	fmt.Print(pterm.LightGreen("What's the sample size in bits (please insert a number divisible by 8)? - default = 2048 bits: "))
 	input_chan := make(chan int, 1)
-	go getInput(input_chan)
+	for sample_size%8 != 0 {
+		fmt.Print(pterm.LightGreen("What's the sample size in bits (please insert a number divisible by 8)? - default = 2048 bits: "))
+		go getInput(input_chan)
 
-	select {
-	case i := <-input_chan:
-		sample_size = i
-		fmt.Printf(pterm.LightYellow("The sample size is %v bits\n"), sample_size)
-	case <-time.After(5000 * time.Millisecond):
-		fmt.Printf(pterm.LightYellow("\nThe sample size is the default value of %v bits\n"), sample_size)
+		select {
+		case i := <-input_chan:
+			sample_size = i
+			fmt.Printf(pterm.LightYellow("The sample size is %v bits\n"), sample_size)
+		case <-time.After(7000 * time.Millisecond):
+			sample_size = 2048
+			fmt.Printf(pterm.LightYellow("\nThe sample size is the default value of %v bits\n"), sample_size)
+		}
+		if sample_size%8 != 0 {
+			fmt.Println(pterm.LightRed("Please insert a number divisible by 8"))
+		}
 	}
 
 	fmt.Print(pterm.LightGreen("What the interval in seconds? - default = 1s: "))
@@ -40,13 +45,14 @@ func main() {
 	case i := <-input_chan:
 		interval_value = i
 		fmt.Printf(pterm.LightYellow("The interval is %v second(s)\n"), interval_value)
-	case <-time.After(5000 * time.Millisecond):
+	case <-time.After(7000 * time.Millisecond):
+		interval_value = 1
 		fmt.Printf(pterm.LightYellow("\nThe interval is the default value of %v second(s)\n"), interval_value)
 	}
 
 	port, err := serial.Open(p, mode)
 	if err != nil {
-		fmt.Println("Error opening port: no Serial found")
+		fmt.Println(pterm.LightRed("Error opening port: no Serial found"))
 		fmt.Println("Starting in Pseudo-Random mode - use for testing only.")
 		device := "pseudo"
 		pseudo_collect(device, sample_size, interval_value)
